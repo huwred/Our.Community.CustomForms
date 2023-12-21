9d671a8818d9474894b966b80f8c456f4dafbb67
huwred <huw@forum.snitz.com>
2023-12-21 09:57:52 +0000

    Update for Umbraco 13

--- Our.Community.CustomForm/Migrations/PackageMigrationPlan.cs
+++ Our.Community.CustomForm/Migrations/PackageMigrationPlan.cs
@@ -1,25 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Examine;
-using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Newtonsoft.Json;
-using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.IO;
-using Umbraco.Cms.Core.Models;
-using Umbraco.Cms.Core.Models.Blocks;
 using Umbraco.Cms.Core.Packaging;
 using Umbraco.Cms.Core.PropertyEditors;
-using Umbraco.Cms.Core.Serialization;
 using Umbraco.Cms.Core.Services;
-using Umbraco.Cms.Core.Services.Implement;
 using Umbraco.Cms.Core.Strings;
 using Umbraco.Cms.Infrastructure.Migrations;
 using Umbraco.Cms.Infrastructure.Packaging;
-using Umbraco.Extensions;
-using static Umbraco.Cms.Core.PropertyEditors.BlockGridConfiguration;
 
 namespace Our.Community.CustomForm.Migrations;
 
